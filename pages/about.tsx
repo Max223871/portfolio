a2ff5f6f4fbc9942dcad9cfae4413cd2e66c4d7b
Max223871 <julianmaxin007@gmail.com>
2024-02-16 14:35:14 +0100

    Initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <h1>About Me</h1>
+    <p>From managing fleet dispatches to diving into the world of web development,
+      my journey has been a testament to my adaptability and passion for learning.
+      As a Junior Full Stack Web Developer, I’ve honed my skills across various technologies,
+      with a special love for front-end development. The thrill of transforming a design into a fully functional,
+      user-friendly interface is what drives me. My experience as a freelancer has further enriched my journey,
+      allowing me to work on diverse projects and continuously learn new things.
+      I pride myself on my ability to quickly pick up new skills and adapt to changing technologies.
+      This journey is not just about changing careers,
+      but also about personal growth and continuous learning.
+      I’m excited about the opportunities that lie ahead in my web development career and look
+      forward to making a significant contribution to the tech industry.</p>
+  </div>
+);
+
+export default About;
---
+++ pages/contact.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Contact = () => (
+  <div>
+    <h1>Contact Me</h1>
+    <p>Hello and welcome to my contact page! I’m thrilled that you’ve taken the time to explore my portfolio.
+      If you have any questions,suggestions, want to discuss a potential project, or simply want to say hello,
+      please feel free to get in touch. I’m always open to new opportunities and look forward to hearing from you.
+      Thank you for your interest in my work!</p>
+
+      <div>
+        <h3>Julian Stanley</h3>
+        <p>Berlin,
+          Germany
+          Phone: +49 1573 181 ****
+          Julianmax0077@gmail.com
+        </p>
+      </div>
+  </div>
+);
+
+export default Contact;
